1767327a9414488d694fc56f770bb6a2ee4a5e7c
mohammedZedd <mohammed.zeddaouii@gmail.com>
2023-12-13 18:53:45 +0100

    version2

---
+++ backend/db/session.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings 
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL 
+print("Databse url is " , SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
+
+SESSIONLOCAL  = sessionmaker(autoflush= False , autpocommit=False , bind=engine)
